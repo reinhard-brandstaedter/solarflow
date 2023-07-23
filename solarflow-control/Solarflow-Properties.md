2d7289625bf264030dce7ffea1bcf8ac656bb07e
Reinhard Weber <r.brandstaedter@gmail.com>
2023-07-23 22:58:14 +0200

    some cleanup

---
+++ solarflow-control/Solarflow-Properties.md
@@ -0,0 +1,16 @@
+### List of properties one can set
+
+Using the topic ```iot/<CLIENT>/<DEVICEID>/properties/write``` with payload
+{"properties": <prop>:<val>}
+one can set:
+
+- inverseMaxPower : the maximum output from the hub to the microinverter
+- outputLimit : the limit output to home
+- inputLimit : the solar input limit (??)
+
+
+### get current property values
+
+One can trigger a property pull by posting a topic to ```iot/<CLIENT>/<DEVICEID>/properties/read``` with payload
+{"properties": [<propertyname>]}
+And then the reply with values can be found in ```iot/<CLIENT>/<DEVICEID>/properties/read/reply```
--- solarflow-control/solarflow.py
+++ solarflow-control/solarflow.py
@@ -3,6 +3,18 @@ from datetime import datetime
 from functools import reduce
 from paho.mqtt import client as mqtt_client
 
+FORMAT = '%(asctime)s:%(levelname)s: %(message)s'
+logging.basicConfig(stream=sys.stdout, level="INFO", format=FORMAT)
+log = logging.getLogger("")
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+logging.getLogger("requests").setLevel(logging.WARNING)
+
+SF_ACCOUNT_ID = os.environ.get('SF_ACCOUNT_ID',None)
+SF_DEVICE_ID = os.environ.get('SF_DEVICE_ID',None)
+
+if SF_ACCOUNT_ID is None or SF_DEVICE_ID is None:
+    log.error(f'Please set SF_ACCOUNT_ID and SF_DEVICE_ID environment variables! Exiting!')
+    sys.exit()
 
 # our MQTT broker where we subscribe to all the telemetry data we need to steer
 # could also be an external one, e.g. fetching SolarFlow data directly from their dv-server
@@ -10,33 +22,27 @@ broker = '192.168.1.245'
 port = 1883
 topic_house = "tele/E220/SENSOR"
 topic_acinput = "inverter/HM-600/ch0/P_AC"
-topic_solarflow = "SKC4SpSn/5ak8yGU7/state"
+topic_solarflow = f'{SF_ACCOUNT_ID}/{SF_DEVICE_ID}/state'
 topic_ahoylimit = "inverter/ctrl/limit/0"
 client_id = f'subscribe-{random.randint(0, 100)}'
 
 # sliding average windows for telemetry data, to remove spikes and drops
-sf_window = 5
+sf_window = int(os.environ.get('SF_WINDOW',5))
 solarflow_values = [0]*sf_window
-sm_window = 10
+sm_window = int(os.environ.get('SM_WINDOW',10))
 smartmeter_values = [0]*sm_window
-inv_window = 5
+inv_window = int(os.environ.get('INV_WINDOW',5))
 inverter_values = [0]*inv_window
 
 
 battery = -1
 charging = 0
-MIN_CHARGE_LEVEL = 125          # The amount of power that should be always reserved for charging, if available. Nothing will be fed to the house if less is produced
-MAX_DISCHARGE_LEVEL = 145       # The maximum discharge level of the battery. Even if there is more demand it will not go beyond that
+MIN_CHARGE_LEVEL = int(os.environ.get('MIN_CHARGE_LEVEL',125))          # The amount of power that should be always reserved for charging, if available. Nothing will be fed to the house if less is produced
+MAX_DISCHARGE_LEVEL = int(os.environ.get('MAX_DISCHARGE_LEVEL',145))    # The maximum discharge level of the battery. Even if there is more demand it will not go beyond that
 OVERAGE_LIMIT = 10              # if we produce more than what we need we can feed that much to the grid
 last_limit = -1                 # just record the last limit to avoid too many calls to inverter API
 last_solar_input_update = datetime.now()
 
-
-FORMAT = '%(asctime)s:%(levelname)s: %(message)s'
-logging.basicConfig(stream=sys.stdout, level="INFO", format=FORMAT)
-log = logging.getLogger("")
-logging.getLogger("urllib3").setLevel(logging.WARNING)
-logging.getLogger("requests").setLevel(logging.WARNING)
 
 def on_solarflow_update(msg):
     global battery, charging
@@ -46,14 +52,14 @@ def on_solarflow_update(msg):
     diff = now - last_solar_input_update
     seconds = diff.total_seconds()
     #if we haven't received any update on solarInputPower we assume it's not producing
-    #log.info(f'No solarInputPower measurement received for {seconds}s')
+    log.info(f'No solarInputPower measurement received for {seconds}s')
     if seconds > 120:
         solarflow_values.pop(0)
         solarflow_values.append(0)
 
     payload = json.loads(msg)
     if "solarInputPower" in payload:
-        if len(solarflow_values) > sf_window:
+        if len(solarflow_values) >= sf_window:
             solarflow_values.pop(0)
         solarflow_values.append(payload["solarInputPower"])
         last_solar_input_update = now
@@ -63,13 +69,13 @@ def on_solarflow_update(msg):
         charging = int((payload["outputPackPower"]))
 
 def on_inverter_update(msg):
-    if len(inverter_values) > inv_window:
+    if len(inverter_values) >= inv_window:
         inverter_values.pop(0)
     inverter_values.append(float(msg))
 
 def on_smartmeter_update(msg):
     payload = json.loads(msg)
-    if len(smartmeter_values) > sm_window:
+    if len(smartmeter_values) >= sm_window:
         smartmeter_values.pop(0)
     smartmeter_values.append(int(payload["Power"]["Power_curr"]))
 
@@ -120,6 +126,8 @@ def steerInverter(client: mqtt_client):
     demand = int(round((smartmeter + inverterinput)))
     limit = 0
 
+    hour = datetime.now().hour
+
     #now all the logic when/how to set limit
     if battery >= 98:
         if solarinput > 0:                                      # to avoid SF from limiting the input we start feeding everythong to the house, just do not get 100% charged
@@ -133,23 +141,34 @@ def steerInverter(client: mqtt_client):
             limit = demand + OVERAGE_LIMIT
         if solarinput > 0 and solarinput < demand:              # producing less than what is needed => take what we can
             limit = solarinput
+        if solarinput > 0 and solarinput <= MIN_CHARGE_LEVEL:   # producing less than the minimum charge level 
+            if hour <= 6 or hour >= 17:                         # in the morning keep using battery
+                limit = MAX_DISCHARGE_LEVEL
+            else:                                               # everything goes to the battery
+                limit = solarinput
         if solarinput <= 0 and demand <= MAX_DISCHARGE_LEVEL:    # not producing and demand is less than discharge limit => discharge with demand
             limit = demand
         if solarinput <= 0 and demand > MAX_DISCHARGE_LEVEL:
             limit = MAX_DISCHARGE_LEVEL
+    elif battery <= 10:                                         # battery is at low stage, stop discharging
+        limit = 0
     else:
         if solarinput > 0 and solarinput > MIN_CHARGE_LEVEL:
             if demand < solarinput - MIN_CHARGE_LEVEL:          # producing more than what is needed => charge more!
-                limit = int(round(demand))
+                limit = demand
             if demand > solarinput - MIN_CHARGE_LEVEL:          # producing less than what is needed => make sure battery is charged with MIN_CHARGE_LEVEL
                 limit = solarinput - MIN_CHARGE_LEVEL
-        if solarinput > 0 and solarinput <= MIN_CHARGE_LEVEL:   # producing less than the minimum charge level => everything goes to the battery
-            limit = 0
+        if solarinput > 0 and solarinput <= MIN_CHARGE_LEVEL:   # producing less than the minimum charge level 
+            if hour <= 6 or hour >= 17:                         # in the morning keep using battery
+                limit = MAX_DISCHARGE_LEVEL
+            else:                                               # everything goes to the battery
+                limit = 0
         if solarinput <= 0 and demand <= MAX_DISCHARGE_LEVEL:   # not producing and the battery is not full => discharge with MAX_DISCHARGE_LEVEL
-            limit = int(round(demand))
+            limit = demand
         if solarinput <= 0 and demand > MAX_DISCHARGE_LEVEL:
             limit = MAX_DISCHARGE_LEVEL
 
+    #log.info(f'History: Demand: {smartmeter_values}, Inverter: {inverter_values}, Solar: {solarflow_values}')
     log.info(f'Demand: {demand}W, Solar: {solarinput}W, Inverter: {inverterinput}W, Battery: {battery}% charging: {charging}W => Limit: {limit}W')
     client.publish(topic_ahoylimit,f'{limit}W')
 
